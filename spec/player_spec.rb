d4882b4d1db1b4522c1459de3423d6d12922c72e
Angela Wolff <warriorwomenblog@gmail.com>
2018-12-04 13:08:27 +0000

    Improving the tests for move method. Moves cumulatively applied to position

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -12,6 +12,13 @@ describe Player do
   describe '#move' do
     it 'a move of 3 spaces changes the players position by 3' do
       expect { player.move(3) }.to change { player.position }.by(3)
+      expect(player.position).to eq 4
+    end
+
+    it 'cumulatively adds to the player position' do
+      player.move(3)
+      player.move(4)
+      expect(player.position).to eq 8
     end
   end
 end
